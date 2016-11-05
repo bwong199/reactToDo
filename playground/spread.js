81d7a9eccacb9b3a37f80133aa633b0adbe1fc58
bwong199 <bwong199@my.bcit.ca>
2016-11-04 20:58:46 -0600

    Add toggle support

--- playground/spread.js
+++ playground/spread.js
@@ -18,18 +18,46 @@
 
 // console.log(final)
 
-var person = ['Andrew', 25];
-var personTwo = ['jen', 29];
+// var person = ['Andrew', 25];
+// var personTwo = ['jen', 29];
 
-function greet(name, age){
-	console.log('Hi ' + name + ', you are ' + age);
+// function greet(name, age){
+// 	console.log('Hi ' + name + ', you are ' + age);
+// }
+// greet(...person);
+// greet(...personTwo);
+
+// var names = ['Mike', 'Ben'];
+// var final = ['Andrew', ...names];
+
+// final.forEach(function(name){
+// 	console.log("Hi " + name)
+// })
+
+function myFunction(arr,  a,  b,  c) 
+{
+	var d = (b + c) / 2;  //3.5
+ 
+	if (b > c) 
+	{
+		return -1;
+	}
+ 
+	if (arr[d] < a) 
+	{
+		return myFunction(arr, a, d+1, c);
+	}
+	else if (arr[d] > a) 
+	{
+		return myFunction(arr, a, b, d-1);
+	}
+	else
+	{
+		return d;
+	}
 }
-greet(...person);
-greet(...personTwo);
 
-var names = ['Mike', 'Ben'];
-var final = ['Andrew', ...names];
+var array = [1,2,3,4,5]
+console.log(myFunction(array,  1, 2, 3))
 
-final.forEach(function(name){
-	console.log("Hi " + name)
-})
+
